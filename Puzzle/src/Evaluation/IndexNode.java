bc99d14a0e8ebaf755d8b821fc302a44794e7e40
tjt69 <tjt69@rutgers.edu>
2017-09-13 14:53:46 -0400

    Whatever I want

---
+++ Puzzle/src/Evaluation/IndexNode.java
@@ -0,0 +1,10 @@
+package Evaluation;
+
+public class IndexNode {
+	int x=-1;
+	int y=-1;
+	public IndexNode(int x, int y){
+		this.x=x;
+		this.y=y;
+	}
+}
---
+++ Puzzle/src/Evaluation/PuzzleGraph.java
@@ -0,0 +1,17 @@
+package Evaluation;
+
+public class PuzzleGraph {
+
+	public PuzzleGraph(int[][] grid, int n){
+		int[][] dis = new int[n][n];
+		
+		for(int i=0; i<n;i++){
+			for(int j=0;j<n;j++){
+				int val = grid[i][j];
+				
+			}
+		}
+	}
+	
+	
+}
