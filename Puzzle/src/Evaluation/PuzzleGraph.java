81d144984154085629d9e39d199c2da03723adff
tjt69 <tjt69@rutgers.edu>
2017-09-13 17:35:16 -0400

    Node

--- Puzzle/src/Evaluation/PuzzleGraph.java
+++ Puzzle/src/Evaluation/PuzzleGraph.java
@@ -3,14 +3,15 @@ package Evaluation;
 public class PuzzleGraph {
 
 	public PuzzleGraph(int[][] grid, int n){
-		int[][] dis = new int[n][n];
+		int[][] visited = new int[n][n];
 		
-		for(int i=0; i<n;i++){
-			for(int j=0;j<n;j++){
-				int val = grid[i][j];
-				
-			}
-		}
+		int val = grid[0][0];
+		
+		PuzzleNode root = new PuzzleNode(val);
+		
+		
+			
+		
 	}
 	
 	
