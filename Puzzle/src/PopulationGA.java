d947449fbf340fd2069f43700f605b59a98d2a13
tjt69 <tjt69@rutgers.edu>
2017-09-26 09:27:24 -0400

    gah

--- Puzzle/src/PopulationGA.java
+++ Puzzle/src/PopulationGA.java
@@ -7,19 +7,22 @@ public class PopulationGA {
 
 		public static MinTurnNode[][] Pop(int size, int n, int it){
 			MinTurnNode[][][] pop = new MinTurnNode [size][n][n];
+			MinTurnNode[][]best= null;
 			 Random rand = new Random();
              int rand1;
             
 			for(int i = 0;i<size; i++) {// Creates initial population
 				
-				EvaluationGrid.setTable(n,n,0,pop[i]);
-				for(int j = 0;j<n-1;j++) {
-					for(int k = 0;k<n-1;k++) {
-						rand1 = rand.nextInt(size);
-			            while(rand1 == 0) {
-			             	rand1 = rand.nextInt(size);
-			            }
-			            EvaluationGrid.setTable(j,k,rand1,pop[i]);
+				EvaluationGrid.setTable(n-1,n-1,0,pop[i]);
+				for(int j = 0;j<n;j++) {
+					for(int k = 0;k<n;k++) {
+						if(!(j==n-1&&k==n-1)) {
+							rand1 = rand.nextInt(size);
+							while(rand1 == 0) {
+								rand1 = rand.nextInt(size);
+							}
+							EvaluationGrid.setTable(j,k,rand1,pop[i]);
+						}
 					}
 				}
 				
@@ -36,10 +39,16 @@ public class PopulationGA {
 			return best;
 		}
 		
-		public static int[] selection(MinTurnNode[][][] pop,int size) {
+		public static int[] selection(MinTurnNode[][][] pop,int size, int n) {
 			int[] parents = new int[size];
 			Random rand = new Random();
 			int one,two;
+			double p1,p2;
+			int totaleval=0;
+			for(int i = 0; i<size; i++) {
+				totaleval += pop[size][n][n].minpath;
+			}
+			
 			
 			for(int i = 0; i<size; i++) {
 				one = rand.nextInt(size);
@@ -47,8 +56,10 @@ public class PopulationGA {
 				while(one== two) {
 					two = rand.nextInt(size);
 				}
-			//turn this into probablities based on best total probablity solution being  ie minpath/ (minpath1+ minpath2+...+minpathn)
-				if(pop[one][size][size].minpath>pop[two][size][size].minpath) {
+				p1 = ((double)pop[one][n][n].minpath)/totaleval;
+				p2 = ((double)pop[two][n][n].minpath)/totaleval;
+				
+				if(p1>p2) {
 					parents[i]=one;
 				}else {
 					parents[i]=two;
@@ -59,9 +70,26 @@ public class PopulationGA {
 			return parents;
 		}
 		
-		public static MinTurnNode[][][] crossOver(int[] parent, MinTurnNode[][][] pop, int size){
+		public static MinTurnNode[][][] crossOver(int[] parent, MinTurnNode[][][] pop, int size, int n){
+			MinTurnNode[][][] nextGen = new MinTurnNode[size][n][n];
+			Random rand = new Random();
+			int rand1 = rand.nextInt(n);
+			int temp;
 			
-			
+			while(rand1==0 || rand1 == n-1) {
+				rand1 = rand.nextInt(n);
+			}
+			if(size%2==0) {
+				for(int i = 0;i<size;i+=2) {
+					for(int j=0;j<n;j++) {
+						for(int k=0;k<rand1;k++) {
+								temp=pop[parent[i]][j][k].value;
+								pop[parent[i]][j][k].value=pop[parent[i+1]][j][k].value;
+								pop[parent[i+1]][j][k].value=temp;
+						}
+					}
+				}
+			}
 			
 			
 			return pop;
